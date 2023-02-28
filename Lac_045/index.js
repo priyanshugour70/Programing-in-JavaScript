603ee93941865870a969d8cd3d528b12d436f248
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-01 00:32:03 +0530

    Lac number 44

---
+++ Lac_045/index.js
@@ -0,0 +1,16 @@
+let first = document.getElementById("first");
+
+first.class = "text-dark yellow";
+first.className = "text-dark red";
+
+first.classList;
+first.classList.remove("red");
+
+first.classList.add("red");
+
+// first.classList.toggle("red")
+// first.classList.toggle("red")
+// first.classList.toggle("red")
+// first.classList.toggle("red")
+
+console.log(first.classList.contains("red"));
