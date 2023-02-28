724a80a7f46b84962274f8d24bf2015b1824e85e
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-02-28 19:56:27 +0530

    Lac number 33

---
+++ Lac_33/index.js
@@ -0,0 +1,9 @@
+// $0 shows the position mean we will click on this tag, and $1 show the last element of an array...
+// $0.nextSibling
+console.log(document.body.firstChild);
+
+a = document.body.firstChild;
+
+console.log(a.parentNode);
+console.log(a.parentElement);
+console.log(a.firstChild.nextSibling);
