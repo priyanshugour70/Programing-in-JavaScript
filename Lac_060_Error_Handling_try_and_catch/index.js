9d2003bfb2eddde2ff2b0a44c10c0e2541fd8533
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-02 17:37:30 +0530

    Error handing try and catch

---
+++ Lac_060_Error_Handling_try_and_catch/index.js
@@ -0,0 +1,25 @@
+setTimeout(() => {
+    console.log("hacking WiFi ... Please Wait..");
+}, 1000);
+
+try {
+    console.log(priyanshu);
+} catch (error) {
+    console.log(
+        error + "  Bhai yah pr error aa rha h too socha ki alert kr du aapko .."
+    );
+}
+
+setTimeout(() => {
+    console.log("Fetching Username and Password... Please wait..");
+}, 2000);
+
+setTimeout(() => {
+    console.log("Hacking Nilesh's Facebook Account..... Please Wait..");
+}, 3000);
+
+setTimeout(() => {
+    console.log(
+        "UserName and Password of Nilesh's (+91 9098393937) Fetched.... Please wait.."
+    );
+}, 4000);
