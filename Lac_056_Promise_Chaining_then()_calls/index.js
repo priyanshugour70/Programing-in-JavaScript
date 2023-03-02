a5afab95c0015c3c349a48a5f925ac8fc4692978
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-02 17:33:00 +0530

    Promise Chaining then()_calls

---
+++ Lac_056_Promise_Chaining_then()_calls/index.js
@@ -0,0 +1,23 @@
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Resolved after 2 sec..");
+        resolve(56);
+    }, 2000);
+});
+
+p1.then((value) => {
+    console.log(value);
+    let p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Promise 2");
+        }, 2000);
+    });
+    return p2;
+})
+    .then((value) => {
+        console.log(value);
+        console.log("We are done..");
+    })
+    .then((value) => {
+        console.log("Now we are pakka done..");
+    });
