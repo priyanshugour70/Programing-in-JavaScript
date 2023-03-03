df15b8475bde18672315e62adcc365e23180f2de
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-03 18:24:10 +0530

    Fetch API

---
+++ Lac_066_Fetch_API/index.js
@@ -0,0 +1,9 @@
+let p = fetch("https://goweather.herokuapp.com/weather/Ny");
+
+p.then((response) => {
+    console.log(response.status);
+    console.log(response.ok);
+    return response.json();
+}).then((value) => {
+    console.log(value);
+});
